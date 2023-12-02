9da01bc313cdc2e8aa6134fe4e90721332be1b46
IS1DI <slavaivankov236@gmail.com>
2023-12-02 15:09:05 +0300

    init

---
+++ src/main/java/com/is1di/remotecoffeemachine/RemoteCoffeeMachineApplication.java
@@ -0,0 +1,13 @@
+package com.is1di.remotecoffeemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteCoffeeMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemoteCoffeeMachineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/is1di/remotecoffeemachine/RemoteCoffeeMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.is1di.remotecoffeemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteCoffeeMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
